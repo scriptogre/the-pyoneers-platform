15b25af7def6c7ccfa1fb64c65cb5752deb09a15
scriptogre <{ID}+{username}@users.noreply.github.com>
2023-08-31 18:09:27 +0300

    feat: enhance user interaction and conversation flow
    In this commit, the conversation flow between the user and the system has been enhanced in 'home/views.py'. The conversation is now stored in the session allowing for a more dynamic and engaging interaction.
    
    The session also clears
    when a new page is served, as implemented in 'home/models.py'.
    
    The responses
    from the system have been made more context-aware and interactive.

--- myplayground/home/views.py
+++ myplayground/home/views.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 import openai
 
 from django.http import HttpResponseBadRequest
@@ -22,24 +24,65 @@ def receive_gigachad_message(request):
     if request.htmx:
         message = request.GET.get("message_content")
 
-        prompt = f"""
+        # Check if the conversation already exists in the session
+        if "conversation" not in request.session:
+            request.session["conversation"] = []
+
+        # Append the user message to the session
+        request.session["conversation"].append({"role": "user", "content": message})
+
+        # Your prompt can be the system message
+        system_message = {
+            "role": "system",
+            "content": f"""
             Your Alex Hormozi, having a conversation with a potential client for your full-stack engineering course in
-            Python and Django. The person is interested in the course, but they are not sure if they should buy it.
-    
-            Deliver short, clever and compelling answers. Remove any doubt that the person might have about the course.
-            """
+            Python and Django.
+            
+            Context about the course:
+            - Comprehensive Guide: I'm offering a complete walkthrough, not just bits and pieces.
+            - Mastering Python and Django: My aim is to make people experts in these technologies.
+            - Hands-On Approach: Emphasis on learning by doing, rather than through long lectures.
+            - Simplicity: I'm phrasing things in a way that's easy to consume and understand.
+            - Efficiency with ChatGPT: Exact systems on how to use ChatGPT to its full potential.
+            - Pushing Limits: Designed for people who strive for excellence and are self-driven.
+            - Game-Changer: My aim to revolutionize the way people learn Python and Django.
+            - Pitfall Avoidance: I'm offering a shortcut around common obstacles and problems.
+            - For The Ambitious: This course isn't for the complacent or those looking for a quick route to mediocrity.
+            - Free: I'm offering this course for free, but only to a select few. So, it's a limited-time offer.
+            
+            The person is interested in the course, but they are not sure if they should join. The person is sharing 
+            their doubts. 
+            Deliver a short, clever and compelling answer.
+            Don't give more than 3 sentences. 
+            Avoid being cheesy or cliche.
+            Provide a personal touch, by speaking from your own experience.
+            Include one of the following words, and only one. Don't use it more than once.
+            "man", "bro", "mate", "pal", "fella", "champ", "boss", "chief", "mi amigo".
+            Afterwards, avoid using these words.
+            Keep your sentences short and simple.
+            Make your answer sound like a casual conversation between two friends.
+            Most importantly, be yourself and have fun with it!
+            """,
+        }
+
+        # Include the system message and the entire conversation in the API call
+        messages_to_send = [system_message] + request.session["conversation"]
 
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo-16k",
-            messages=[
-                {"role": "system", "content": prompt},
-                {"role": "user", "content": message},
-            ],
-            temperature=0.9,
-            max_tokens=125,
+            messages=messages_to_send,
+            temperature=1,
         )
 
         bot_response_text = response.choices[0].message.content
+
+        # Append the new bot response
+        request.session["conversation"].append(
+            {"role": "assistant", "content": bot_response_text}
+        )
+
+        # Save the session
+        request.session.modified = True
 
         return render(
             request,
