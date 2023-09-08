c1b3f5f15f8f8e5c883156b3446a42aafe7b6ea7
scriptogre <{ID}+{username}@users.noreply.github.com>
2023-09-08 14:31:50 +0300

    feat(settings): add pyoneers.dev as allowed host in production settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,3 +6,9 @@ try:
     from .local import *
 except ImportError:
     pass
+
+ALLOWED_HOSTS = [
+    "the-pyoneers-platform-service.onrender.com",  # Your Render subdomain
+    "pyoneers.dev",  # Your custom domain
+    "www.pyoneers.dev",  # Optional: if you also want to allow www subdomain
+]
