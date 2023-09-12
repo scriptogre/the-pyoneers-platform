6798b989e17da27fafeb3bd8a7a25cb010aa74ec
scriptogre <{ID}+{username}@users.noreply.github.com>
2023-09-12 10:47:40 +0300

    feat(local): Implement Celery task propagation setting

--- config/settings/local.py
+++ config/settings/local.py
@@ -62,9 +62,8 @@ INSTALLED_APPS += ["django_extensions"]  # noqa: F405
 
 # Celery
 # ------------------------------------------------------------------------------
-# TODO: Implement Celery
-# # http://docs.celeryproject.org/en/latest/userguide/configuration.html#task-eager-propagates
-# CELERY_TASK_EAGER_PROPAGATES = True
+# http://docs.celeryproject.org/en/latest/userguide/configuration.html#task-eager-propagates
+CELERY_TASK_EAGER_PROPAGATES = True
 
 # wagtail
 # ------------------------------------------------------------------------------
