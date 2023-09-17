a68fd507aca70921bc92876034c07685c6b2d662
scriptogre <{ID}+{username}@users.noreply.github.com>
2023-09-17 15:59:39 +0300

    feat: Refactor home page layout and update golden card HTML template
    Change the home page layout to a new structure. Rename `home_page.html` to `home.html` and move it to 'templates/home/'. Split the page into multiple sections and move each section to 'templates/home/partials/'. Add a new golden card HTML template to 'templates/home/partials/'.

--- pyoneers_platform/home/views.py
+++ pyoneers_platform/home/views.py
@@ -12,7 +12,7 @@ from config.settings.base import DISCORD_BOT_TOKEN, DISCORD_GUILD_ID
 
 
 class HomeView(TemplateView):
-    template_name = "home_page.html"
+    template_name = "home/home.html"
 
     def get(self, request, *args, **kwargs):
         if "conversation" in request.session:
@@ -50,10 +50,10 @@ def receive_gigachad_message(request):
         system_message = {
             "role": "system",
             "content": f"""
-            You're Alex Hormozi, engaged in a casual conversation with a potential student who's on the fence about 
-            joining your comprehensive full-stack engineering course in Python and Django. 
+            You're Alex Hormozi, engaged in a casual conversation with a potential student who's on the fence about
+            joining your comprehensive full-stack engineering course in Python and Django.
             The person seems interested but is throwing up various excuses for not taking the plunge.
-            
+
             Context about the course:
             - In-Depth Guide: This isn't your run-of-the-mill course; it's a complete journey from beginner to pro in Python and Django.
             - Expertise Guaranteed: The goal is to make participants masters of Python and Django.
@@ -67,7 +67,7 @@ def receive_gigachad_message(request):
             - Free Material, Forever: The course material is free for everyone, forever. No strings attached.
             - Big Offer: I'm offering a free unlimited 1-on-1 mentorship to 5 students who are serious about learning.
             - My 100% Effort: I'm juggling with a full-time job yet still putting in 8-10 hours a day into this course.
-            
+
             Your Task:
             - Be persuasive but genuine and down-to-earth in your response.
             - Speak from your personal experience to make your point.
@@ -75,7 +75,7 @@ def receive_gigachad_message(request):
             - Use straightforward language; keep it short and sweet.
             - Avoid clichés and cheesiness.
             - If it fits naturally, include one of the following terms: "man", "mate", "pal", "fella", "champ", "boss", "chief".
-            
+
             This is your moment to turn their indecision into action.
             """,
         }
